5a2b56c52c3a84d4e294fbc7f5815f320dd182a8
Arockiaraj <arockiaraj1994@gmail.com>
2016-10-03 19:17:17 +0530

    message servlet changes

--- src/com/news/server/Message.java
+++ src/com/news/server/Message.java
@@ -23,11 +23,9 @@ public class Message {
 	public void setCreateon(Date createon) {
 		this.createon = createon;
 	}
+	
 	public String getId() {
 		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
 	}
 	public String getUser() {
 		return user;
--- src/com/news/server/MessageServlet.java
+++ src/com/news/server/MessageServlet.java
@@ -1,11 +1,15 @@
 package com.news.server;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 
 /**
@@ -38,7 +42,7 @@ public class MessageServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		Message message = new Message();
-		
+		JSONObject jsonObject = (JSONObject) request.getAttribute("messagedata");
 		
 		String insertRowsCount = Message.saveMessage(message);
 		response.getWriter().write(insertRowsCount);
