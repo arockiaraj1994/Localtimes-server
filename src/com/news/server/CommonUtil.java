5ccd5c663f1099b915440ac8710218c60d6c3ab6
Arockiaraj <arockiaraj1994@gmail.com>
2016-10-03 17:19:27 +0530

    Trying connect mysql db

--- src/com/news/server/CommonUtil.java
+++ src/com/news/server/CommonUtil.java
@@ -1,6 +1,9 @@
 package com.news.server;
 
+import java.sql.DriverManager;
 import java.util.UUID;
+
+import java.sql.Connection;
 
 public class CommonUtil {
 	
@@ -8,6 +11,18 @@ public class CommonUtil {
 		UUID uuid = UUID.randomUUID();
 		System.out.println(uuid.toString());
 		return uuid.toString();
+	}
+	public static Connection getConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3030/newsapp","root","january1994");
+			return connection;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 	public static void main(String[] test){
 		getPrimaryKey();
--- src/com/news/server/MessageServlet.java
+++ src/com/news/server/MessageServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import netscape.javascript.JSObject;
 
 /**
  * Servlet implementation class MessageServer
